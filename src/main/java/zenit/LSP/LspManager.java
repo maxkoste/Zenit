529f22ceac715ea65ee7c26bf1c2923119afb0a1
Max Koste <60856309+maxkoste@users.noreply.github.com>
2026-02-25 11:49:29 +0100

    Starting with a writer and a reader to set up the tools needed for communicating with the language server

--- src/main/java/zenit/LSP/LspManager.java
+++ src/main/java/zenit/LSP/LspManager.java
@@ -1,68 +1,83 @@
 package zenit.LSP;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class LspManager {
 	private String serverPath;
 	private File workspace;
+	private OutputStream stdin;
+	private BufferedWriter writer;
 
-	public LspManager(){
+	public LspManager() {
 		this.serverPath = "/Users/maxkoste/Dev/Java/jdt-language-server";
-		this.workspace =new File( System.getProperty("user.home"));
+		this.workspace = new File(System.getProperty("user.home"));
 	}
 
-	public void setWorkspace(File workspace){
+	public void setWorkspace(File workspace) {
 		if (workspace != null) {
 			this.workspace = workspace;
 		} else {
-			//set defualt.
-			this.workspace =new File( System.getProperty("user.home"));
+			// set defualt.
+			this.workspace = new File(System.getProperty("user.home"));
 		}
 	}
 
-  public Process startServer() throws IOException {
+	public Process startServer() throws IOException {
 		System.out.println("[DEBUG] Starting LSP Server from LSPManager");
 
-        File baseDir = new File(serverPath);
-        File pluginsDir = new File(baseDir, "plugins");
+		File baseDir = new File(serverPath);
+		File pluginsDir = new File(baseDir, "plugins");
 
-        if (!pluginsDir.exists()) {
-            throw new RuntimeException("Invalid JDT LS path: plugins folder not found");
-        }
+		if (!pluginsDir.exists()) {
+			throw new RuntimeException("Invalid JDT LS path: plugins folder not found");
+		}
 
-        // Find equinox launcher jar
-        File launcherJar = null;
-        for (File file : pluginsDir.listFiles()) {
-            if (file.getName().startsWith("org.eclipse.equinox.launcher_")
-                    && file.getName().endsWith(".jar")) {
-                launcherJar = file;
-                break;
-            }
-        }
+		// Find equinox launcher jar
+		File launcherJar = null;
+		for (File file : pluginsDir.listFiles()) {
+			if (file.getName().startsWith("org.eclipse.equinox.launcher_")
+					&& file.getName().endsWith(".jar")) {
+				launcherJar = file;
+				break;
+			}
+		}
 
-        if (launcherJar == null) {
-            throw new RuntimeException("Launcher jar not found in plugins folder");
-        }
+		if (launcherJar == null) {
+			throw new RuntimeException("Launcher jar not found in plugins folder");
+		}
 
 		String workspacePath = workspace.getAbsolutePath();
 
-        ProcessBuilder pb = new ProcessBuilder(
-                "java",
-                "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-                "-Dosgi.bundles.defaultStartLevel=4",
-                "-Declipse.product=org.eclipse.jdt.ls.core.product",
-                "-Dlog.protocol=true",
-                "-Dlog.level=ALL",
-                "-jar", launcherJar.getAbsolutePath(),
-                "-configuration", baseDir.getAbsolutePath() + "/config_mac",
-                "-data", workspacePath
-        );
+		ProcessBuilder pb = new ProcessBuilder(
+			"java",
+			"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+			"-Dosgi.bundles.defaultStartLevel=4",
+			"-Declipse.product=org.eclipse.jdt.ls.core.product",
+			"-Dlog.protocol=true",
+			"-Dlog.level=ALL",
+			"-jar", launcherJar.getAbsolutePath(),
+			"-configuration", baseDir.getAbsolutePath() + "/config_mac",
+			"-data", workspacePath);
 
-        pb.directory(baseDir);
-        pb.inheritIO(); // show logs in console
+		pb.directory(baseDir);
+		pb.inheritIO(); // show logs in console
 
-        return pb.start();
-    }
-	
+		Process p = pb.start();
+		this.stdin = p.getOutputStream();
+		this.writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+		return p;
+	}
+
+	public OutputStream getStdin() {
+		return this.stdin;
+	}
+
+	public BufferedWriter getWriter() {
+		return this.writer;
+	}
 }
